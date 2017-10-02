d3e41d1f3006fbd60f5610354ca27fd03438af6f
Carlos Arana Matus <camanarac@gmail.com>
2017-10-02 18:16:22 -0500

    Commit 20171002
    Limpieza de datos de accidentes vehiculares por municipio. Se agregaron microdatos (incidentes undividuales) desde archivos.dbf hacia archivo .xlsx a nivel municipal (Numero de accidentes por municipio).
    (Archivo https://github.com/INECC-PCCS/MV02/MV02.xlsx)

--- 00_Parametros/MV02/MV02.py
+++ 00_Parametros/MV02/MV02.py
@@ -1,47 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 25 11:44:16 2017
+Created on Thu Sep 28 13:34:56 2017
 
 @author: carlos.arana
+
+Descripcion: Mineria de datos de accidentes vehiculares.
+
 """
-
-'''
-Descripcion: Mineria de datos de vehiculos registrados.
-
-'''
 
 # Librerias Utilizadas
 import os
-import pandas
+import pandas as pd
 import datetime
-import numpy as np
 import urllib
 import zipfile
+from simpledbf import Dbf5
 
 # Ubicacion y descripcion de la fuente
 fuente = r'http://www.beta.inegi.org.mx/proyectos/registros/economicas/accidentes/'
-describe_fuente = 'Informacion disponible en http://sc.inegi.org.mx/cobdem/ ' \
-                  '\n > Ruta: ' \
-                  '\n > Proyecto e indice de contenidos ' \
-                  '\n > Aprovechamiento de registros administrativos > Estadísticas económicas' \
-                  '\n > Estadísticas de transporte ' \
-                  '\n > Accidentes de tránsito terrestre en zonas urbanas y suburbanas (1997 - 2015)' \
-                  '\n > Consultar información de: > Total de accidentes de tránsito' \
-                  '\n > Pestaña 1. Variables [Seleccionar "Zona donde ocurrió el accidente"]' \
-                  '\n > Pestaña 2. Años a consultar [Seleccionar Todos]' \
-                  '\n > Pestaña 3. Área geográfica [Ver Municipios] [Seleccionar Todos]' \
-                  '\n > [Actualizar Consulta]' \
-                  '\n > [Expandir Columnas]' \
-                  '\n > [Expandir renglones]' \
-                  '\n > [Exportar datos a Excel]'
-
+describe_fuente = ''
 fecha_mineria = datetime.datetime.now()
-
 
 # Descargar bases de datos de accidentes
 dir_local_base = r'D:\PCCS\00_RawData\01_CSV\Accidentes'
 dir_local_sub = r'\zip'
 URLBASE = 'http://www.beta.inegi.org.mx/contenidos/proyectos/registros/economicas/accidentes/microdatos'
+dataset_destino = 'MV02'
 
 URLS = [
     r'/1997/atus_97_dbf.zip', r'/1998/atus_98_dbf.zip', r'/1999/atus_99_dbf.zip', r'/2000/atus_00_dbf.zip',
@@ -87,23 +71,31 @@ for k, v in unzipdirs.items():
             print('Done: {}.{} --- {} --- {}'.format(x, k, v, file))
     x +=1
 
+DataSet = pd.concat(dbfs.values(), keys=dbfs.keys())
 
-for k, v in dbfs.items():
-    print('***\n{}\n{}\n{}'.format(k, list(v), len(v)))
+# Asignar clave geoestadistica municipal estandar de 5 digitos
+Estado = DataSet['EDO'].apply(lambda x: str(x).zfill(2)).map(str)
+Municipio = DataSet['MPIO'].apply(lambda x: str(x).zfill(3))
+DataSet['CVE_MUN'] = Estado + Municipio
 
+# Consolidar numero de accidentes por municipio y por año
+DataSet.set_index('CVE_MUN', append=True, inplace=True)
+DataSet.reset_index(level=1, inplace=True)
+DataSet_b = DataSet['MPIO'].groupby(level=[0,1]).count()
 
+# Hacer columnas para cada año
+Dataset_c = DataSet_b.unstack(level=0)
 
 # Descripcion del dataset
 descripcion = {
-    'Nombre del Dataset'   : 'Vehiculos de motor registrados en circulación',
-    'Descripcion del dataset' : 'Numero de Vehiculos de motor registrados en circulación. Incluye automoviles,'
-                                'Camiones para pasajeros, Camiones y camionetas para carga y motocicletas',
-    'Fuente'    : 'SIMBAD - Sistema Estatal y municipal de Base de Datos (INEGI)',
-    'URL_Fuente': 'http://sc.inegi.org.mx/cobdem/',
+    'Nombre del Dataset'   : 'Accidentes de tránsito en zonas urbanas y suburbanas',
+    'Descripcion del dataset' : 'Accidentes de tránsito en zonas urbanas y suburbanas. Información anual y municipal',
+    'Fuente'    : 'INEGI (Microdatos)',
+    'URL_Fuente': fuente,
     'Obtencion de dataset' : describe_fuente,
     'Desagregacion' : 'Municipal',
-    'Disponibilidad temporal' : '1980 a 2016',
-    'Repositorio de mineria' : 'https://github.com/INECC-PCCS/MV01',
+    'Disponibilidad temporal' : '1997 a 2015',
+    'Repositorio de mineria' : 'https://github.com/INECC-PCCS/{}'.format(dataset_destino),
     'Notas' : 'S/N'
 }
 
@@ -112,52 +104,8 @@ metadatos = pandas.DataFrame.from_dict(descripcion, orient='index')
 metadatos = metadatos.rename(columns = {0:'Descripcion'})
 
 # Exportar a excel
-writer = pandas.ExcelWriter(r'D:\PCCS\01_Dmine\00_Parametros\MV01\MV01.xlsx')
-dataset_b.to_excel(writer, sheet_name = 'DATOS')
+writer = pandas.ExcelWriter(r'D:\PCCS\01_Dmine\00_Parametros\{}\{}.xlsx'.format(dataset_destino, dataset_destino))
+Dataset_c.to_excel(writer, sheet_name = 'DATOS')
 metadatos.to_excel(writer, sheet_name = 'Metadatos')
 writer.close()
 
-
-'''
-Codigo de pruebas 
-'''
-
-# Librerias utilizadas
-import pandas as pd
-from simpledbf import Dbf5
-
-# Descargar y descomprimir la base de datos del Sistema Urbano Nacional en formato SHP
-dir_local_base = r'D:\PCCS\00_RawData\01_CSV\Accidentes'
-dir_local_sub = r'\zip'
-localfile = 'atus_15_dbf.zip'
-destino = dir_local+dir_local_sub+'\\'+localfile
-URL = r'http://www.beta.inegi.org.mx/contenidos/proyectos/registros/economicas/accidentes/microdatos/2015/atus_15_dbf.zip'
-
-
-
-os.getcwd()
-
-zip_ref = zipfile.ZipFile(elzip, 'r')
-zip_ref.extractall(dir_local)
-
-SUN_MAIN = r'D:\PCCS\01_Dmine\00_Parametros\sun_main.csv'
-
-# Convertir datos descargados como dbf a Pandas Dataframe.
-sundbf = Dbf5(Archivo, codec='mbcs')
-sun_df = sundbf.to_dataframe()
-
-# Cargar subsistema principal
-SUN_MAIN = pd.read_csv(r'D:\PCCS\01_Dmine\00_Parametros\sun_main.csv',
-                      dtype={'CVE_SUN': str,
-                             'CVE_ENT': str,
-                             'CVE_MUN': str,
-                             'CVE_LOC': str,
-                             'CVE_SUNMUN': str},
-                      encoding='UTF-8',
-                      )
-
-# Clasificar por subsistema principal
-ciudades = sun_df['CVE_SUN']
-ciudades = ciudades.apply(lambda x: str(x).zfill(3))
-listamain = SUN_MAIN['CVE_SUN'].tolist()
-sun_df['SS_PPAL'] = ciudades.apply((lambda x: x in listamain))
